6504a7a7efea64c16ba19379652fa3342ff834d9
pineapple-apk <studee.512@gmail.com>
2026-08-21 01:06:09 +0530

    L1.c

--- L1.c
+++ L1.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("pineapple");
+    int a, b, c;
+
+    printf("enter value of a: ");
+    scanf("%d", &a);
+
+    printf("enter value of b: ");
+    scanf("%d", &b);
+
+    printf("\nsum of a and b is: ");
+    c = a + b;
+    printf("%d", c);
+
+    //puts("drink water");
+
+    // c = a + b;
 
     return 0;
 }
