0c500ea03068ae4cc85b1819a1da0ae72106c335
pineapple-apk <studee.512@gmail.com>
2026-09-08 03:52:53 -0700

    Update L3.c

--- L3.c
+++ L3.c
@@ -4,5 +4,17 @@ int main() {
     float a=6.7;
     a=7.6;
     printf("%f",a);
+    const char x='m';
+    //x='n';
+    printf("%c",x);
+    
+    char l[4] ="xyz";
+    const char m[4] ="abc";
+    printf("%s%s",l,m);
+
+    //string p="xyz";
+    //const string q="abc";
+    //printf("%s%s",p,q);
 
     return 0;
+}
