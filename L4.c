68bf03b0c65a36ab3cc207caa39dc7b980dc50a5
pineapple-apk <studee.512@gmail.com>
2026-09-08 22:07:30 -0700

    Update L4.c

--- L4.c
+++ L4.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+
+void sum(){
+    int c=5;
+    printf("%d",c);
+}
+
+int a= 5;
 int main() {
-    printf("pineapple");
+    // Write C code here
+    int d=7,e=6;
+    printf("%d%d%d",e,d,a);
+    
+    sum();
 
     return 0;
 }
