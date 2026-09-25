320f6a590bf4d88c9b9a7bfd60c9384a975b1f1f
pineapple-apk <studee.512@gmail.com>
2026-09-25 10:05:43 +0530

    Create L7.c

---
+++ L7.c
@@ -0,0 +1,62 @@
+#include<studio.h>
+
+int main(){
+int m;
+
+printf("enter month: ");
+scanf("%d",&m);
+
+if(m<13){
+
+if(m=1){
+printf("January");
+}
+
+if(m=1){
+printf("Fabruary");
+}
+
+if(m=1){
+printf("March");
+}
+
+if(m=1){
+printf("April");
+}
+
+if(m=1){
+printf("May");
+}
+
+if(m=1){
+printf("June");
+}
+
+if(m=1){
+printf("July");
+}
+
+if(m=1){
+printf("Agust");
+}
+
+if(m=1){
+printf("September");
+}
+
+if(m=1){
+printf("October");
+}
+
+if(m=1){
+printf("November");
+}
+
+if(m=1){
+printf("December");
+}}
+else{
+printf("baarh se upar nahi lala");
+}
+ return 0;
+}
