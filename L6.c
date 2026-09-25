c608e92b144596b0bef7835502b0615157c85ff3
pineapple-apk <studee.512@gmail.com>
2026-09-25 09:42:39 +0530

    Create L6.c

---
+++ L6.c
@@ -0,0 +1,37 @@
+#include<studio.h>
+int i=1
+
+int main(){
+int d;
+printf("enter day");
+scanf("%d",&day);
+
+if(d==1+7i){
+printf("Monday")
+};
+
+if(d==2+7i){
+printf("Tuesday")
+};
+
+if(d==3+7i){
+printf("Wednesday")
+};
+
+if(d==4+7i){
+printf("Thursday")
+};
+
+if(d==5+7i){
+printf("Friday")
+};
+
+if(d==6+7i){
+printf("Saturday")
+};
+
+if(d==7+7i){
+printf("Sunday")
+};
+
+return 0;
