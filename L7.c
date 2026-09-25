746be1ddf6250cd8dd52dca01f9c62221051de76
pineapple-apk <studee.512@gmail.com>
2026-09-25 10:10:11 +0530

    Update L7.c

--- L7.c
+++ L7.c
@@ -12,52 +12,52 @@ if(m==1){
 printf("January");
 }
 
-if(m==1){
+if(m==2){
 printf("Fabruary");
 }
 
-if(m==1){
+if(m==3){
 printf("March");
 }
 
-if(m==1){
+if(m==4){
 printf("April");
 }
 
-if(m==1){
+if(m==5){
 printf("May");
 }
 
-if(m==1){
+if(m==6){
 printf("June");
 }
 
-if(m==1){
+if(m==7){
 printf("July");
 }
 
-if(m==1){
+if(m==8){
 printf("Agust");
 }
 
-if(m==1){
+if(m==9){
 printf("September");
 }
 
-if(m==1){
+if(m==10){
 printf("October");
 }
 
-if(m==1){
+if(m==11){
 printf("November");
 }
 
-if(m==1){
+if(m==12){
 printf("December");
 }}
 
 else{
-printf("baarh se upar nahi lala");
+printf("baarh se upar nahi ");
 }
 return 0;
 }
