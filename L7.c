a1dd59d2b82e35b6310fd7c5a3f3dd724a5e2053
pineapple-apk <studee.512@gmail.com>
2026-09-25 10:07:16 +0530

    Update L7.c

--- L7.c
+++ L7.c
@@ -1,4 +1,4 @@
-#include<studio.h>
+#include<stdio.h>
 
 int main(){
 int m;
@@ -8,55 +8,56 @@ scanf("%d",&m);
 
 if(m<13){
 
-if(m=1){
+if(m==1){
 printf("January");
 }
 
-if(m=1){
+if(m==1){
 printf("Fabruary");
 }
 
-if(m=1){
+if(m==1){
 printf("March");
 }
 
-if(m=1){
+if(m==1){
 printf("April");
 }
 
-if(m=1){
+if(m==1){
 printf("May");
 }
 
-if(m=1){
+if(m==1){
 printf("June");
 }
 
-if(m=1){
+if(m==1){
 printf("July");
 }
 
-if(m=1){
+if(m==1){
 printf("Agust");
 }
 
-if(m=1){
+if(m==1){
 printf("September");
 }
 
-if(m=1){
+if(m==1){
 printf("October");
 }
 
-if(m=1){
+if(m==1){
 printf("November");
 }
 
-if(m=1){
+if(m==1){
 printf("December");
 }}
+
 else{
 printf("baarh se upar nahi lala");
 }
- return 0;
+return 0;
 }
